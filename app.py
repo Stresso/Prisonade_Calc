7da180fc1789c7badb3d6e31f78aa7144be2c0ef
Illusioner27 <115498492+Illusioner27@users.noreply.github.com>
2023-03-14 09:25:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -195,9 +195,9 @@ elif dim=="Citadel":
           if ((hr % 64)==0):
                st.header(f"You need {int(hr/64):,d} Stacks of T3 Obsidian")
           else:
+          mg = val * 80
           fg = val * 184
           hr = val * 64
-          mg = val *80
           if ((mg % 64)==0):
                st.header(f"You need {int(mg/64):,d} Stacks of Purpur")
           else:
